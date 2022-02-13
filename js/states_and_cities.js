5d9493686268ab2cc234c45b90f4a32eabb16a5c
Nimish Lohumi <91203793+Ace-09@users.noreply.github.com>
2022-02-13 12:19:08 +0530

    t

---
+++ js/states_and_cities.js
@@ -0,0 +1,27 @@
+var citiesByState = {
+    Odisha: ["Bhubaneswar", "Puri", "Cuttack"],
+    Maharashtra: ["Mumbai", "Pune", "Nagpur"],
+    MadhyaPradesh: ["Indore"],
+    Kerala: ["kochi", "Kanpur"],
+    UttarPradesh: ["Lucknow"],
+    Uttarakhand: ["Dehradun", "Kashipur"]
+}
+function makeSubmenu(value) {
+    if (value.length == 0) document.getElementById("citySelect").innerHTML = "<option></option>";
+    else {
+        var citiesOptions = "";
+        for (cityId in citiesByState[value]) {
+            citiesOptions += "<option>" + citiesByState[value][cityId] + "</option>";
+        }
+        document.getElementById("citySelect").innerHTML = citiesOptions;
+    }
+}
+function displaySelected() {
+    var country = document.getElementById("countrySelect").value;
+    var city = document.getElementById("citySelect").value;
+    alert(country + "\n" + city);
+}
+function resetSelection() {
+    document.getElementById("countrySelect").selectedIndex = 0;
+    document.getElementById("citySelect").selectedIndex = 0;
+}
